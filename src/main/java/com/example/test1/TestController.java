85e10fa8acb052d6f3bca11730680ff116c4ff4d
MJiason <48732069+MJiason@users.noreply.github.com>
2021-05-14 01:12:34 +0300

    Update TestController.java

--- src/main/java/com/example/test1/TestController.java
+++ src/main/java/com/example/test1/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public ResponseEntity<String> hello() {
-        return new ResponseEntity<>("helloWord1", HttpStatus.OK);
+        return new ResponseEntity<>("helloWord2", HttpStatus.OK);
     }
 }
