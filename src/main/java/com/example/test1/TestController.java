b27d65e9e8bd8c76c96afc88c778ac05a48b7b19
MJiason <ovr01110@gmail.com>
2021-05-13 12:55:19 +0300

    fix

--- src/main/java/com/example/test1/TestController.java
+++ src/main/java/com/example/test1/TestController.java
@@ -2,11 +2,12 @@ package com.example.test1;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
+    @GetMapping("/")
     public ResponseEntity<String> hello() {
         return new ResponseEntity<>("hello", HttpStatus.OK);
     }
